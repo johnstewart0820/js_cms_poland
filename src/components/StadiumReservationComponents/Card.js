99886d2378956f36665a08d284cfb9948430062e
johnstewart0820 <john.s@outsource.codes>
2020-08-11 10:32:16 +0200

    card warning fix

--- src/components/StadiumReservationComponents/Card.js
+++ src/components/StadiumReservationComponents/Card.js
@@ -4,45 +4,46 @@ import ButtonLink from "../buttons/ButtonLink";
 import ShareButton from "../buttons/ShareButton";
 import {  PlusIcon } from "../../svg/icons";
 import '../../styles/StadiumReservationPages/Card.scss';
+import {useHistory} from 'react-router-dom';
 
 const Card = ({id, name, title, address, thumbnail, postCode ,city, greenButtonOnclick}) => {
+    const history = useHistory();
+
     return(
         <Col>
-            <a href={`/reservation/${id}`}>
-                <div className="card">
-                    <div className="card__thumbnail has-overlay" style={{backgroundImage: `url(${thumbnail})`}}>
-                        <div className="card__name">
-                            {name}
-                        </div>
+            <div className="card">
+                <div className="card__thumbnail has-overlay" style={{backgroundImage: `url(${thumbnail})`}}>
+                    <div className="card__name">
+                        {name}
                     </div>
-
-                    <div className="card__content">
-                        <div className="card__title">
-                            {title}
-                        </div>
-
-                        {address && postCode && city &&
-                            <div className="card__address">
-                                <span> ADRES </span>
-                                <Row>
-                                    <h3> {postCode} {city}</h3>
-                                </Row>
-                                <Row>
-                                    <h3>{address}</h3>
-                                </Row>
-                            </div>
-                        }
-
-                        <div className="card__bottom">
-                            <ButtonLink extra_classes="green" onClick={greenButtonOnclick}> REZERWACJA </ButtonLink>
-
-                            <a href="#"> <PlusIcon/> </a>
-                            <ShareButton link_for_sharing={`${window.location.origin}/reservation/${id}`}/>
-                        </div>
-                    </div>
-
                 </div>
-            </a>
+
+                <div className="card__content">
+                    <div className="card__title">
+                        {title}
+                    </div>
+
+                    {address && postCode && city &&
+                        <div className="card__address">
+                            <span> ADRES </span>
+                            <Row>
+                                <h3>{postCode} {city}</h3>
+                            </Row>
+                            <Row>
+                                <h3>{address}</h3>
+                            </Row>
+                        </div>
+                    }
+
+                    <div className="card__bottom">
+                        <ButtonLink extra_classes="green" onClick={greenButtonOnclick}> REZERWACJA </ButtonLink>
+
+                        <a href="#"> <PlusIcon/> </a>
+                        <ShareButton link_for_sharing={`${window.location.origin}/reservation/${id}`}/>
+                    </div>
+                </div>
+
+            </div>
         </Col>
     )
 }
