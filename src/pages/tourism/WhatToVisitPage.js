b6adcd686ac76e53f948c7f9568cfe67bd7062a7
johnstewart0820 <john.s@outsource.codes>
2020-08-13 09:57:16 +0200

    add paths to WhatToVisitPage

--- src/pages/tourism/WhatToVisitPage.js
+++ src/pages/tourism/WhatToVisitPage.js
@@ -8,15 +8,18 @@ import LoopSearchForm from "../../components/loop/LoopSearchForm";
 import LoopSearchPostsContainer from "../../components/loop/LoopSearchPostsContainer";
 import Loader from "../../components/general/Loader";
 import LoopAttractionPost from "../../components/attractions/LoopAttractionPost";
+import LoopPathPost from "../../components/paths/LoopPathPost";
 
 export default function () {
     const [slides, setSlides] = React.useState([]);
     const [attractions, setAttractions] = React.useState(null);
-    
+    const [paths, setPaths] = React.useState(null);
+
     React.useEffect(() => {
         setTimeout(() => {
             setSlides(sample_slides);
-            API.get("mock/attractions.json").then( res => setAttractions(res.data));
+            API.get("mock/attractions.json").then(res => setAttractions(res.data));
+            API.get("mock/paths.json").then(res => setPaths(res.data));
         }, 500);
     }, []);
 
@@ -33,10 +36,16 @@ export default function () {
                 submitCallback={() => {}}
             />
 
-            <LoopSearchPostsContainer extra_classes={'gastronomy'} heading={'Atrakcje'} headingLink={window.location} headingLinkText={'ZOBACZ WSZYSTKIE'}>
-                {attractions !== null && !attractions.length && <h3>Brak </h3>}
-                {!!attractions?.length && attractions?.map((item, index) => <LoopAttractionPost key={ index } {...item } />)}
-                {!attractions && <Loader style={{ width: "100%" }} />}
+            <LoopSearchPostsContainer extra_classes={'gastronomy'} heading={'Atrakcje'} headingLink={window.location.href} headingLinkText={'ZOBACZ WSZYSTKIE'}>
+                {attractions !== null && !attractions.length && <h3>Brak</h3>}
+                {!!attractions?.length && attractions?.map((item, index) => <LoopAttractionPost key={index} {...item} />)}
+                {!attractions && <Loader style={{width: '100%'}} />}
+            </LoopSearchPostsContainer>
+
+            <LoopSearchPostsContainer extra_classes={'gastronomy'} heading={'Szlaki'} headingLink={window.location.href} headingLinkText={'ZOBACZ WSZYSTKIE'}>
+                {paths !== null && !paths.length && <h3>Brak</h3>}
+                {!!paths?.length && paths?.map((item, index) => <LoopPathPost key={index} {...item} />)}
+                {!paths && <Loader style={{ width: '100%' }} />}
             </LoopSearchPostsContainer>
         </>
     );
