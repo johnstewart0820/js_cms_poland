6edc6cf4d306288e4265efe9eb9ef6e3a51fe53d
johnstewart0820 <john.s@outsource.codes>
2020-08-19 16:08:36 +0200

    USTRON-24 WIP Added confirmation page to events

---
+++ src/pages/RegisterToEvent/RegisterToEventConfirmation.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {useHistory} from "react-router-dom";
+import Loader from "../../components/general/Loader";
+import '../../styles/StadiumReservationPages/ReservationConfirmation.scss';
+import ButtonLink from "../../components/buttons/ButtonLink";
+import {Container} from "../../components/UserPanel/Container";
+import TourismRoutes from "../../constants/TourismRoutes";
+
+const RegisterToEventConfirmationPage = () => {
+    const history = useHistory();
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+
+    const [subscriptionData, setSubscriptionData] = React.useState(null);
+
+
+
+    return (
+        <Container
+            containerTitle={'REJESTRACJA NA ZAWODY'}
+        >
+            <div className="description">
+                <div className="description__inner">
+                    {!subscriptionData && <Loader/>}
+                    {!!subscriptionData && (
+                        <>
+                            <h1>
+                                Pomyślnie zarejestrowano na wybrane wydarzenia<br/>
+                            </h1>
+                            <h3>
+                                Wpisowe w wysokości 10 zł od osoby prosimy przesyłać na konto ING nr 85<br/>
+                                10501070 1000 2126 6545 456 455 z podaniem imion i nazwisk osob, których<br/>
+                                dotyczy opłata
+                            </h3>
+                            <ButtonLink extra_classes="green" onClick={() => history.push(TourismRoutes.ReservationHistoryPage)}>ZAREJESTRUJ</ButtonLink>
+                        </>
+                    )}
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default RegisterToEventConfirmationPage;
