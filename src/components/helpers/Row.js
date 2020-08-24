b7a925c18b7f9f88f4e7da906a3ce176536c4d8e
johnstewart0820 <john.s@outsource.codes>
2020-08-24 18:16:04 +0200

    Added row title

--- src/components/helpers/Row.js
+++ src/components/helpers/Row.js
@@ -2,9 +2,16 @@ import React from "react";
 import '../../styles/helpers/row.scss';
 
 const Row = props => (
-    <div className='row'>
-        {props.children}
-    </div>
+    <>
+        {props.rowTitle && (
+            <div className='row'>
+                <h3 className='row-title'>{props.rowTitle.toUpperCase()}</h3>
+            </div>
+        )}
+        <div className='row'>
+            {props.children}
+        </div>
+    </>
 )
 
 export default Row;
