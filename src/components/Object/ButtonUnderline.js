da9c3f3b1c3f222a5963f83d0f9b8521c4a2ec03
johnstewart0820 <john.s@outsource.codes>
2020-08-26 14:26:32 +0200

    WIP USTRON-26 Added button with underline

---
+++ src/components/Object/ButtonUnderline.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../../styles/ObjectPages/ButtonUnderline.scss';
+import PropTypes from 'prop-types';
+
+const ButtonUnderline = props => {
+    return(
+        <>
+            <button
+                onClick={props.onClick}
+                className="button-with-underline">
+                {props.buttonText.toUpperCase()}
+                <hr/>
+            </button>
+        </>
+    )
+}
+
+ButtonUnderline.propTypes = {
+    buttonText: PropTypes.string.isRequired,
+}
+
+export default ButtonUnderline;
