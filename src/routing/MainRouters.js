c253842ae0db785638246b6278ed887dae2e25be
johnstewart0820 <john.s@outsource.codes>
2020-09-02 11:22:04 +0300

    MAIN site add news,events single; Sidebar changing height

--- src/routing/MainRouters.js
+++ src/routing/MainRouters.js
@@ -4,9 +4,17 @@ import asyncComponent from "../components/AsyncComponent";
 
 const StartPage = asyncComponent(() => import( "../pages/StartPage" ) );
 
+const EventsPage = asyncComponent(() => import('../pages/Events/EventsPage'));
+const EventSinglePage = asyncComponent( () => import( "../pages/common/EventSinglePage" ));
+
+const NewsPage = asyncComponent( () => import( "../pages/common/NewsPage" ));
+const NewsSinglePage = asyncComponent( () => import( "../pages/common/NewsSinglePage" ));
+
 const MainRouters = () => (
 	<>
-		<Route component={ StartPage } />
+		<Route exact path="/" component={ StartPage } />
+		<Route exact path="/news/:id" component={ NewsSinglePage } />
+		<Route exact path="/events/:id" component={ EventSinglePage } />
 	</>
 )
 
