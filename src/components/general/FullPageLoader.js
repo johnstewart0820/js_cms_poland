5a2e3b9cec9fddca8380acd4f8a69c3441da816c
johnstewart0820 <john.s@outsource.codes>
2020-09-04 16:04:37 +0300

    FullPageLoader fix for more smooth

--- src/components/general/FullPageLoader.js
+++ src/components/general/FullPageLoader.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Loader from "./Loader";
 import "../../styles/general/full-page-loader.scss";
 
 import { SiteInfoContextConsumer } from "../../constants/SiteInfoContext"; 
 
-const FullPageLoader = ({ extra_classes, children }) => (
+const FullPageLoader = ({ children }) => (
 	<SiteInfoContextConsumer>
 		{ ({ site_info_loading }) => (
-			<>
-				 
-				<div className={`full-page-loader ${ site_info_loading ? "" : "hidden" }`}>
+			<>				 
+				<div className={`full-page-loader ${ !site_info_loading && "hidden" }`}>
 					<Loader />
 				</div>
 				
-	
 				{ !site_info_loading && children }
 			</>
 		)}
 	</SiteInfoContextConsumer>
 )
-
-FullPageLoader.propTypes = { }
 
 export default FullPageLoader;
