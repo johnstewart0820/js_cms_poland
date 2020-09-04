f59c664617bbe979163a885f24da1b9f61ec318b
johnstewart0820 <john.s@outsource.codes>
2020-09-04 16:25:29 +0200

    Initial structure for planer history component

---
+++ src/components/PlanerList/PlanerHistory.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../../styles/PlanerList/PlanerHistory.scss';
+
+const PlanerHistory = () => {
+    return (
+        <div className="planer-history-container">
+            <div className="planer-history-total-counter">
+                <h4>
+
+                </h4>
+                <h2>
+
+                </h2>
+                <hr/>
+                <h5>
+
+                </h5>
+            </div>
+            <div className="planer-history">
+
+            </div>
+            <div className="planer-history-button">
+
+            </div>
+        </div>
+    )
+}
+
+export default PlanerHistory;
