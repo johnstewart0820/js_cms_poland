f49102381ec52d8964e5da58d59223d61d6bab8c
johnstewart0820 <john.s@outsource.codes>
2020-09-15 16:09:21 +0200

    update 404 page stub

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import PageHeaderSection from "../components/header/PageHeaderSection";
+import MainHeaderSection from "../components/header/MainHeaderSection";
 
 export default function NotFoundPage(props) {
     return (
-        <div>
-            404
-        </div>
+        <MainHeaderSection style={{backgroundImage: 'url("/img/forest.png")', height: '100%'}}>
+            <PageHeaderSection >
+                <div className="container">
+                    <div className="page-title">
+                        404<br/>
+                        Strona o podanym adresie nie została znaleziona
+                    </div>
+                </div>
+            </PageHeaderSection>
+        </MainHeaderSection>
     );
 }
