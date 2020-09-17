de8ddfddd79df19e66006f4ea090afcca23e260c
johnstewart0820 <john.s@outsource.codes>
2020-09-17 11:35:09 +0200

    Routing.js added hardcoded pages

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -7,6 +7,23 @@ import MainRouters from "./MainRouters";
 import TourismRouters from "./TourismRouters";
 import PageRenderer from "../extra/PageRenderer";
 import NotFoundPage from "../pages/NotFoundPage";
+import TourismRoutes from "../constants/TourismRoutes";
+import PlanerListPage from "../pages/common/PlanerListPage";
+import LoginPage from "../pages/UserPanelPages/LoginRegistration/LoginPage";
+import RegistrationPage from "../pages/UserPanelPages/LoginRegistration/RegistrationPage";
+import RegistrationConfirmationPage from "../pages/UserPanelPages/LoginRegistration/RegistrationConfirmationPage";
+import ActivateAccountPage from "../pages/UserPanelPages/LoginRegistration/ActivateAccountPage";
+import CourtsWithCardsPage from "../pages/UserPanelPages/StadiumReservation/CourtsWithCardsPage";
+import UserProfilePage from "../pages/UserPanelPages/UserProfile/UserProfilePage";
+import ReservationConfirmationPage from "../pages/UserPanelPages/StadiumReservation/ReservationConfirmationPage";
+import ReservationCalendarPage from "../pages/UserPanelPages/StadiumReservation/ReservationCalendarPage";
+import ReservationHistoryPage from "../pages/UserPanelPages/StadiumReservation/ReservationsHistoryListPage";
+import RegisterToEventListPage from "../pages/UserPanelPages/RegisterToEvent/RegisterToEventListPage";
+import RegisterToEventConfirmationPage from "../pages/UserPanelPages/RegisterToEvent/RegisterToEventConfirmationPage";
+import ObjectListPage from "../pages/UserPanelPages/Objects/ObjectsListPage";
+import EditObjectFormPage from "../pages/UserPanelPages/Objects/EditObjectFormPage";
+import GameCardsPage from "../pages/UserPanelPages/Game/GameCardsPage";
+import RegisterToEventFormPage from "../pages/UserPanelPages/RegisterToEvent/RegisterToEventFormPage";
 
 const ROUTERS = {
     "MAIN": <MainRouters/>,
@@ -15,10 +32,36 @@ const ROUTERS = {
 
 const Routing = () => (
     <Switch>
-		<Route exact path={'/'} component={PageRenderer}/>
-		<Route exact path={'/page/:pageId'} component={PageRenderer}/>
+        {/* Planer list */}
+        <Route exact path={TourismRoutes.PlanerListPage} component={PlanerListPage}/>
+
+        {/* Authorization pages */}
+        <Route exact path={TourismRoutes.Login} component={LoginPage} />
+        <Route exact path={TourismRoutes.Registration} component={RegistrationPage} />
+        <Route exact path={TourismRoutes.RegistrationConfirmation} component={RegistrationConfirmationPage} />
+        <Route exact path={TourismRoutes.ActivateAccount} component={ActivateAccountPage}/>
+
+        {/* User panel pages */}
+        <Route exact path={TourismRoutes.UserProfile} component={UserProfilePage}/>
+        <Route exact path={TourismRoutes.StadiumReservation} component={CourtsWithCardsPage}/>
+        <Route exact path={TourismRoutes.ReservationConfirmationPage} component={ReservationConfirmationPage}/>
+        <Route exact path={TourismRoutes.Reservation()} component={ReservationCalendarPage}/>
+        <Route exact path={TourismRoutes.ReservationHistoryPage} component={ReservationHistoryPage}/>
+        <Route exact path={TourismRoutes.RegisterToEventList} component={RegisterToEventListPage}/>
+        <Route exact path={TourismRoutes.RegisterToEventForm} component={RegisterToEventFormPage}/>
+        <Route exact path={TourismRoutes.RegisterToEventConfirmationPage} component={RegisterToEventConfirmationPage}/>
+        <Route exact path={TourismRoutes.ObjectListPage} component={ObjectListPage}/>
+        <Route exact path={TourismRoutes.EditObjectFormPage} component={EditObjectFormPage}/>
+        <Route exact path={TourismRoutes.GameCardsPage} component={GameCardsPage}/>
+
+        {/* Routes for pages which controlled from CMS */}
+        <Route exact path={'/'} component={PageRenderer}/>
+        <Route exact path={'/page/:pageId'} component={PageRenderer}/>
         <Route exact path={'/:slug'} component={PageRenderer}/>
+
+        {/* 404 page */}
         <Route component={NotFoundPage}/>
+
         {ROUTERS[SITE]}
     </Switch>
 );
