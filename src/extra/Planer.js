7941adcf5ca44d9fdb913dc8206e4a062d18bf9f
johnstewart0820 <john.s@outsource.codes>
2020-09-21 16:57:37 +0200

    Fix for planer fix :)

--- src/extra/Planer.js
+++ src/extra/Planer.js
@@ -4,6 +4,6 @@ export default {
     saveData : data => localStorage.setItem(LocalStorage.Planer, JSON.stringify(data)),
     getData : () => {
         const item = localStorage.getItem(LocalStorage.Planer);
-        return item ? JSON.parse(item) : null;
+        return item ? JSON.parse(item) : [];
     },
 };
