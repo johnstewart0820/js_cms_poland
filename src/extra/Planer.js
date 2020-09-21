db161f86b0f773b20268e3d3fbe8e432f93717f5
johnstewart0820 <john.s@outsource.codes>
2020-09-21 15:44:29 +0200

    Planer context getter fix

--- src/extra/Planer.js
+++ src/extra/Planer.js
@@ -2,5 +2,8 @@ import LocalStorage from "../constants/LocalStorage";
 
 export default {
     saveData : data => localStorage.setItem(LocalStorage.Planer, JSON.stringify(data)),
-    getData : () => JSON.parse(localStorage.getItem(LocalStorage.Planer)),
+    getData : () => {
+        const item = localStorage.getItem(LocalStorage.Planer);
+        return item ? JSON.parse(item) : null;
+    },
 };
