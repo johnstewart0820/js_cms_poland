eae1adcbd293c3a3a394db8bb8af6b1c1ffc8e1e
johnstewart0820 <john.s@outsource.codes>
2020-09-24 15:13:33 +0200

    Added onClick props

--- src/components/GamePagesComponents/SingleGamePageButtonsContainer.js
+++ src/components/GamePagesComponents/SingleGamePageButtonsContainer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ButtonLink from "../buttons/ButtonLink";
 
-const SimpleGamePageButtonsContainer = ({})=>{
+const SimpleGamePageButtonsContainer = ({qrButtonOnClick, visitButtonOnClick}) => {
     const visitButton = "potwierdz wizytę GPS";
     const qrButton = "skanuj Qr kod";
 
     return(
         <div className={'buttons-category'}>
-            <button className="button-link green full-width">{qrButton.toUpperCase()}</button>
-            <ButtonLink extra_classes="green-transparent" >{visitButton.toUpperCase()}</ButtonLink>
+            <button className="button-link green full-width" onClick={qrButtonOnClick}>{qrButton.toUpperCase()}</button>
+            <ButtonLink extra_classes="green-transparent" onClick={visitButtonOnClick}>{visitButton.toUpperCase()}</ButtonLink>
         </div>
     );
 }
