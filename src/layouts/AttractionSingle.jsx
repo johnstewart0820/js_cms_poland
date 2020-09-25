b06c6c407f65b8c1844e977383c83591fdfae332
johnstewart0820 <john.s@outsource.codes>
2020-09-25 12:31:29 +0200

    Revert "create AttractionSinglePage"
    This reverts commit 72ab924b

--- src/layouts/AttractionSingle.jsx
+++ src/layouts/AttractionSingle.jsx
@@ -1,10 +1,10 @@
-
 import React from 'react';
 import Parser from "html-react-parser";
 import MainHeaderSection from "../components/header/MainHeaderSection";
 import OneCarouseInRow from "../components/carousel/OneCarouseInRow";
 import Gallery from "../components/gallery/Gallery";
 import Breadcrumbs from "../components/general/Breadcrumbs";
+import NewsSingleHead from "../components/news/NewsSingleHead";
 import LoopNewsPost from "../components/news/LoopNewsPost";
 import SingleContainer from "../components/common-single/SingleContainer";
 import SingleContentBottom from "../components/common-single/SingleContentBottom";
@@ -12,6 +12,7 @@ import {API} from "../extra/API";
 import axios from "../extra/axios";
 import Loader from "../components/general/Loader";
 import AttractionSingleHead from "../components/attractions/AttractionSingleHead";
+import {Container} from "../components/UserPanel/Container";
 import '../styles/attractions/AttractionSinglePage.scss'
 
 const pageId = 100;
@@ -44,3 +45,54 @@ export default function AttractionSingle(props) {
     if (!!loading)
         return <Loader/>
 
+
+
+    // React.useEffect(() => {
+    //     API.getEntities({categories: props.page.categories})
+    //         .then(res => setNews(res.data.contents))
+    //         .catch(e => {
+    //             console.error(e);
+    //             setNews(false);
+    //         });
+    // }, []);
+
+
+    const a = props.page.gallery.map(r=> {r.name = r.name})
+    console.log(props.page.gallery[0])
+
+    return (
+        <>
+            <MainHeaderSection extra_classes="single">
+                <Breadcrumbs breadcrumbs={[]}/>
+                <AttractionSingleHead {...props.page}/>
+            </MainHeaderSection>
+
+            <SingleContainer extra_classes="single-news-container">
+                {/*<div>{Parser(props.page.acf.field_description)}</div>*/}
+                <SingleContentBottom/>
+            </SingleContainer>
+
+
+
+            <div className="page-title">
+                <img alt="" src={require('../svg/icons/logo-black.svg')}/>
+                <h3>
+                OBSŁUGA W JĘZYKU
+                </h3>
+            </div>
+
+
+            <Gallery items={a}/>
+
+            
+
+
+            <OneCarouseInRow carousel={{
+                loading: news === null,
+                heading: 'Ostatnie aktualności',
+                ItemComponent: LoopNewsPost,
+                items: news || [],
+            }} />
+        </>
+    );
+};
