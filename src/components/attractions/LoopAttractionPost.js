05be30a34a4033f3fdd2995f39829837a47b493f
johnstewart0820 <john.s@outsource.codes>
2020-10-01 12:34:28 +0200

    fix LoopAttractionPost

--- src/components/attractions/LoopAttractionPost.js
+++ src/components/attractions/LoopAttractionPost.js
@@ -7,21 +7,21 @@ import DisabilityIcon from "../icons/DisabilityIcon";
 import FamilyIcon from "../icons/FamilyIcon";
 import PlanerContext from "../../constants/PlanerContext";
 import DefaultImage from "../../constants/DefaultImage";
+import {getArticleLink} from "../../extra/functions";
+import {Link} from "react-router-dom";
 
-export default function ({id, title, excerpt, category, thumbnail}) {
+export default function (attraction) {
     const planerContext = React.useContext(PlanerContext);
 
     return (
         <div className="loop-card">
-            <a
-                href={`/attractions/${id}`}
-                target={'_blank'}
-                rel={'noopener noreferrer'}
+            <Link
+                to={getArticleLink(attraction)}
                 className="loop-card__thumbnail has-overlay thumbnail"
-                style={{backgroundImage: `url("${thumbnail || DefaultImage}")`}}
+                style={{backgroundImage: `url("${attraction.image || DefaultImage}")`}}
             >
-                <div className="loop-card__thumbnail-text">{category}</div>
-            </a>
+                <div className="loop-card__thumbnail-text">{attraction.categories_labels}</div>
+            </Link>
 
             <div className="loop-card__content-container">
                 <div className="my-3 loop-card__icons">
@@ -30,16 +30,16 @@ export default function ({id, title, excerpt, category, thumbnail}) {
                     <FamilyIcon/>
                 </div>
                 <div className="loop-card__title heading">
-                    <a href={`/attractions/${id}`} target={'_blank'} rel={'noopener noreferrer'}>
-                        {title}
-                    </a>
+                    <Link to={getArticleLink(attraction)}>
+                        {attraction.title}
+                    </Link>
                 </div>
 
-                {excerpt && <div className="loop-card__text">{excerpt}</div>}
+                {attraction.short && <div className="loop-card__text">{attraction.short}</div>}
 
                 <div className="loop-card__action-buttons">
-                    <PlusButton onClick={() => planerContext.add(id)}/>
-                    <ShareButton link_for_sharing={`${window.location.origin}/attractions/${id}`}/>
+                    <PlusButton onClick={() => planerContext.add(attraction.id)}/>
+                    <ShareButton link_for_sharing={window.location.origin + getArticleLink(attraction)}/>
                 </div>
             </div>
         </div>
