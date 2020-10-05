6d546a7554c458fd8d18251334d09141b8f08241
johnstewart0820 <john.s@outsource.codes>
2020-10-05 13:36:29 +0200

    TourismSpa layout WIP

--- src/components/carousel/OneCarouseInRow.js
+++ src/components/carousel/OneCarouseInRow.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import Carousel from "./Carousel";
 import Loader from "../general/Loader";
-
 import "../../styles/carousel/one-carousel-in-row.scss";
 
-const OneCarouseInRow = ({ loading, carousel }) => (
-	<section className="one-carousel-in-row">
-		{ loading && <Loader /> }
-		
-		<div>
-			{ !loading && <Carousel { ...carousel } /> }
-		</div>
-	</section>
-)
+const OneCarouseInRow = ({loading, carousel}) => (
+    <section className="one-carousel-in-row">
+        {loading && <Loader/>}
+
+        <div>
+            {!loading && <Carousel {...carousel} />}
+        </div>
+    </section>
+);
 
 OneCarouseInRow.propTypes = {
-	loading: PropTypes.bool,
-	carousel: PropTypes.object.isRequired
-}
+    loading: PropTypes.bool,
+    carousel: PropTypes.object.isRequired,
+};
 
 export default OneCarouseInRow;
