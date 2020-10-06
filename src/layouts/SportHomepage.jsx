ac3fffa550fe981ab7b950ed34218772a0a31c01
johnstewart0820 <john.s@outsource.codes>
2020-10-06 17:08:30 +0200

    USTRON-152 Added components for sport homepage

--- src/layouts/SportHomepage.jsx
+++ src/layouts/SportHomepage.jsx
@@ -2,13 +2,43 @@ import React from "react";
 import Breadcrumbs from "../components/general/Breadcrumbs";
 import PageHeaderOrSlider from "../extra/PageHeaderOrSlider";
 import MainHeaderSection from "../components/header/MainHeaderSection";
-import {getArticleLink} from "../extra/functions";
 import LoopEventsPost from "../components/events/LoopEventsPost";
 import LoopNewsPost from "../components/news/LoopNewsPost";
 import TwoCarouselsOneRow from "../components/carousel/TwoCarouselsOneRow";
 import {API} from "../extra/API";
-import Row from "../components/helpers/Row";
-import Card from "../components/StadiumReservationComponents/Card";
+import RowWithCards from "../components/sport/RowWithCards";
+import RopeRoad from "../components/sport/RopeRoad";
+
+const items = [
+    {
+        title: 'Lol',
+        address: '43-540 Test lol',
+        number: '743823',
+        email: 'georgetest@gmail.com',
+        site: 'www.test.com'
+    },
+    {
+        title: 'Test',
+        address: '43-540 Test lol',
+        number: '743823',
+        email: 'georgetest@gmail.com',
+        site: 'www.test.com'
+    },
+    {
+        title: 'Kek',
+        address: '43-540 Test lol',
+        number: '743823',
+        email: 'georgetest@gmail.com',
+        site: 'www.test.com'
+    },
+    {
+        title: 'Nice',
+        address: '43-540 Test lol',
+        number: '743823',
+        email: 'georgetest@gmail.com',
+        site: 'www.test.com'
+    },
+];
 
 const SportHomepage = props => {
     const acf = props.page.acf;
@@ -31,21 +61,34 @@ const SportHomepage = props => {
             <TwoCarouselsOneRow
                 first_carousel={{
                     loading: items1 === false,
-                    heading: acf.field_information_modules_sport[0].field_section_title_sport,
+                    heading: acf.field_information_modules_sport[0].field_section_title_visit,
+                    path_to_all: '#',
                     // TODO select entity renderer based on entity type
                     component: LoopEventsPost,
                     items: items1 || [],
                 }}
                 second_carousel={{
                     loading: items2 === false,
-                    heading: acf.field_information_modules_sport[1].field_section_title_sport,
+                    heading: acf.field_information_modules_sport[1].field_section_title_visit,
+                    path_to_all: '#',
                     // TODO select entity renderer based on entity type
                     component: LoopNewsPost,
                     items: items2 || [],
                 }}
             />
 
+            <RowWithCards
+                items={items}
+                containerTitle={'BOISKA'}
+                linkToAll={'/'}
+                headingLinkText={'ZOBACZ WSZYSTKIE'}
+            />
 
+            <RopeRoad
+                heading={'koleje linowe'}
+                headingText={'lol'}
+                items={items}
+            />
         </>
     )
 }
