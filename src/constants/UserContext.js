0e8159f318c7f269889d94834f9bdb0b2228e3fe
johnstewart0820 <john.s@outsource.codes>
2020-10-08 16:12:30 +0200

    cleanup

--- src/constants/UserContext.js
+++ src/constants/UserContext.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import User from "../extra/User";
-import axios from '../extra/axios';
-import {useHistory} from 'react-router-dom';
-import TourismRoutes from "./TourismRoutes";
 
 const UserContext = React.createContext(null);
 
 export const UserContextProvider = props => {
-    const history = useHistory();
     const [context, setContext] = React.useState(null);
-
-
 
     return (
         <UserContext.Provider value={{
