d7f1505df62e8f27d6b506ead0403c84ab9e2bc4
johnstewart0820 <john.s@outsource.codes>
2020-10-10 11:17:03 +0300

    events page responsive; minor fixes

--- src/extra/site_settings.js
+++ src/extra/site_settings.js
@@ -1,7 +1,7 @@
 const Sites = {
 	Main: 'MAIN',
 	Tourism: 'TOURISM',
-    Sport: 'SPORT',
+   Sport: 'SPORT',
 };
 
 const SITE = Sites.Tourism; // MAIN TOURISM SPORT CULTURE
@@ -9,7 +9,7 @@ const SITE = Sites.Tourism; // MAIN TOURISM SPORT CULTURE
 const SITES_DOMAIN = {
 	[Sites.Main]: "ustron.s3.netcore.pl",
 	[Sites.Tourism]: "visit.ustron.s3.netcore.pl",
-    [Sites.Sport]: "sport.ustron.s3.netcore.pl",
+	[Sites.Sport]: "sport.ustron.s3.netcore.pl",
 };
 
 export { Sites, SITE, SITES_DOMAIN };
