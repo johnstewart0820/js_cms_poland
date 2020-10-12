a3e8f95194f33f397d4859d37aa7522f9a7abb39
johnstewart0820 <john.s@outsource.codes>
2020-10-12 10:49:45 +0200

    fix <Link> not handling external URL by adding <SelectiveLink>

---
+++ src/extra/SelectiveLink.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export default function SelectiveLink({to, children, ...props}) {
+    // It is a simple element with nothing to link to
+    if (!to) return <span {...props}>{children}</span>;
+
+    // It is intended to be an external link
+    if (/^https?:\/\//.test(to)) return <a href={to} {...props}>{children}</a>;
+
+    // Finally, it is an internal link
+    return <Link to={to} {...props}>{children}</Link>;
+};
