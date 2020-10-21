6134ee21d18fa2e29ba3313b6596dce6c5bec7b9
johnstewart0820 <john.s@outsource.codes>
2020-10-21 16:59:39 +0200

    USTRON-178 final add attachment to head in news singlepage

--- src/layouts/NewsSingle.jsx
+++ src/layouts/NewsSingle.jsx
@@ -62,7 +62,6 @@ export default function NewsSinglePage(props) {
                 </SingleContainer>
             )}
 
-
             {props.page.video.length !== 0 &&
             <Video video={props.page.video.embed}/>}
             {props.page.attachments.length !== 0 && <Attachment attachments={props.page.attachments}/>}
