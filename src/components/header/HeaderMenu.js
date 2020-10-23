44aba0ba3156d2a967f3c7eb9fbcd73941f57800
johnstewart0820 <john.s@outsource.codes>
2020-10-23 17:32:23 +0200

    add active classes to header menu links

--- src/components/header/HeaderMenu.js
+++ src/components/header/HeaderMenu.js
@@ -2,14 +2,32 @@ import React from 'react';
 import SiteInfoContext from "../../constants/SiteInfoContext";
 import SelectiveLink from "../../extra/SelectiveLink";
 
+const isActive = (url, breadcrumbs) => {
+    if (!Array.isArray(breadcrumbs))
+        return false;
+
+    for (const crumb of breadcrumbs) {
+        if ((new URL(crumb.url)).pathname === url)
+            return true;
+    }
+
+    return false;
+};
+
 const HeaderMenu = () => {
-    const {header_menu} = React.useContext(SiteInfoContext);
+    const {header_menu, page} = React.useContext(SiteInfoContext);
     return (
         <>
             {header_menu && !!header_menu.length && (
                 <div className="header-main__menu">
                     {header_menu.map(({path, label}, index) => (
-                        <SelectiveLink key={index} to={path}>{label}</SelectiveLink>
+                        <SelectiveLink
+                            key={index}
+                            to={path}
+                            className={isActive(path, page?.breadcrumb) ? 'active' : ''}
+                        >
+                            {label}
+                        </SelectiveLink>
                     ))}
                 </div>
             )}
