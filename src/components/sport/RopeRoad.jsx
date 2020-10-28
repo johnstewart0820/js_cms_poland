330a9ba60ce96dee0f199f2a28336c5889c80fa5
johnstewart0820 <john.s@outsource.codes>
2020-10-28 11:17:48 +0300

    RopeRoad and RopeRoadCard fixes responsive

--- src/components/sport/RopeRoad.jsx
+++ src/components/sport/RopeRoad.jsx
@@ -3,33 +3,42 @@ import SectionHeading from "../general/SectionHeading";
 import RopeRoadCard from "./RopeRoadCard";
 import '../../styles/sport/RopeRoad.scss';
 
-const RopeRoad = ({heading, headingText, items}) => {
+const RopeRoad = ({ heading, headingText, items }) => {
     return (
-        <div className='rope-road-row'>
-            <div className='rope-road-head'>
-                <SectionHeading
-                    heading={heading}
-                    headingText={headingText}
-                />
-            </div>
-            <div className='rope-road-body'>
-                {items?.length > 0 && items.map((item, index) => {
-                    const newItem = item.acf;
+        <section className="rope-road">
+            
+				<div className="container"> 
 
-                    if (index + 1 > 3)
-                        return;
+					<div className="row"> 
 
-                    return(
-                        <RopeRoadCard
-                            key={index}
-                            {...item}
-                            {...newItem}
-                            article={item}
-                        />
-                    )
-                })}
-            </div>
-        </div>
+						<SectionHeading heading={ heading } />
+
+						{ headingText && <div className="rope-road__text"> { headingText } </div> }
+						
+						<div className='rope-road__body'>
+							{ items && !!items.length && 
+								items.map(( item, index ) => {
+								const newItem = item.acf;
+
+								if ( index + 1 > 3 )
+										return null;
+
+								return(
+										<RopeRoadCard
+											key={index}
+											{...item}
+											{...newItem}
+											article={item}
+										/>
+								)
+							})}
+						</div>
+
+					</div>
+					
+				</div>
+				
+        </section>
     )
 }
 
