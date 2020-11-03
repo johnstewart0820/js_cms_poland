8a643d276e90643aa4bdd1fe97d8a9370ba2b540
johnstewart0820 <john.s@outsource.codes>
2020-11-03 11:55:28 +0300

    FullPageLoader shows while switch pages

--- src/components/general/FullPageLoader.js
+++ src/components/general/FullPageLoader.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Loader from "./Loader";
 import "../../styles/general/full-page-loader.scss";
 
-const FullPageLoader = () => (
-    <div className={'full-page-loader'}>
-        <Loader/>
-    </div>
+const FullPageLoader = props => (
+   <>
+		<div className={`full-page-loader ${ props.extra_classes || "" }`}>
+			<Loader/>
+		</div>
+
+		{ props.children }
+	</>
 );
 
 export default FullPageLoader;
